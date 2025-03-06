b5479e8585d0c6376eda4c75fdb780f7fb225c3e
Zalatanchik1 <ai.golubevsanya@gmail.com>
2025-03-07 01:18:41 +0400

    Добавил 2 позитивных теста: - test_create_booking_with_random_data - test_create_booking_with_empty_data_field Добавил 2 негативных теста (на статус код 500): - test_create_booking_with_empty_json - test_create_booking_with_incorrect_format

--- test/test_create_booking.py
+++ test/test_create_booking.py
@@ -1,4 +1,6 @@
 import allure
+import pytest
+from requests import HTTPError
 
 from core.models.booking import BookingResponse
 from pydantic import ValidationError
@@ -34,8 +36,8 @@ def test_create_booking_with_custom_data(api_client):
 
 
 @allure.feature('Test creating booking')
-@allure.story('Positive: creating booking with custom data and bookingdates')
-def test_create_booking_with_custom_data_and_bookingdates(api_client, generate_random_booking_data):
+@allure.story('Positive: creating booking with random data')
+def test_create_booking_with_random_data(api_client, generate_random_booking_data):
     response = api_client.create_booking(generate_random_booking_data)
     try:
         BookingResponse(**response)
@@ -49,3 +51,60 @@ def test_create_booking_with_custom_data_and_bookingdates(api_client, generate_r
     assert response['booking']['bookingdates']['checkin'] == generate_random_booking_data['bookingdates']['checkin']
     assert response['booking']['bookingdates']['checkout'] == generate_random_booking_data['bookingdates']['checkout']
     assert response['booking']['additionalneeds'] == generate_random_booking_data['additionalneeds']
+
+
+@allure.feature('Test creating booking')
+@allure.story('Positive: creating booking with empty_data_field')
+def test_create_booking_with_empty_data_field(api_client):
+    booking_data = {
+        "firstname" : "",
+        "lastname" : "",
+        "totalprice" : 133,
+        "depositpaid" : True,
+        "bookingdates" : {
+            "checkin" : "2020-01-01",
+            "checkout" : "2019-01-01"
+        },
+        "additionalneeds" : ""
+    }
+    response = api_client.create_booking(booking_data)
+    try:
+        BookingResponse(**response)
+    except ValidationError as e:
+        raise ValidationError(f"Response validation failed{e}")
+
+    assert response['booking']['firstname'] == booking_data['firstname']
+    assert response['booking']['lastname'] == booking_data['lastname']
+    assert response['booking']['totalprice'] == booking_data['totalprice']
+    assert response['booking']['depositpaid'] == booking_data['depositpaid']
+    assert response['booking']['bookingdates']['checkin'] == booking_data['bookingdates']['checkin']
+    assert response['booking']['bookingdates']['checkout'] == booking_data['bookingdates']['checkout']
+    assert response['booking']['additionalneeds'] == booking_data['additionalneeds']
+
+
+@allure.feature('Test creating booking')
+@allure.story('Negative: creating booking with empty json')
+def test_create_booking_with_empty_json(api_client):
+    with pytest.raises(HTTPError):
+        booking_data = {}
+        response = api_client.create_booking(booking_data)
+        return response
+
+@allure.feature('Test creating booking')
+@allure.story('Negative: creating booking with incorrect format')
+def test_create_booking_with_incorrect_format(api_client):
+    with pytest.raises(HTTPError):
+        booking_data = {
+        "firstname" : 123,
+        "lastname" : 333,
+        "totalprice" : 111,
+        "depositpaid" : True,
+        "bookingdates" : {
+            "checkin" : "2020-01-01",
+            "checkout" : "2019-01-01"
+        },
+        "additionalneeds" : ""
+    }
+        response = api_client.create_booking(booking_data)
+        return response
+
